ae6266cd5c2b5fc161e06477cd7493b09c82bba6
Yesarib <yesaribaris23@gmail.com>
2025-05-16 17:08:23 +0300

    feat: add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
+import Footer from "@/components/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,10 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="min-h-screen flex flex-col mb-16">
+        <div className="min-h-screen flex flex-col">
           <Header />
           <main className="flex-1 mt-16"> {children} </main>
-
+          <Footer />
         </div>
       </body>
     </html>
