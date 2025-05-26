0bf48897a625a5927d652a7c019938c57e52bea1
Yesarib <yesaribaris23@gmail.com>
2025-05-26 18:44:01 +0300

    feat: add subscription datas on subscription page

--- src/sections/subscription/subscription-hero.tsx
+++ src/sections/subscription/subscription-hero.tsx
@@ -1,22 +1,18 @@
 import { Card, CardContent } from "@/components/ui/card";
+import { Subscription } from "@/types/subscription";
+import { User } from "@/types/user";
+import { formatDate } from "@/utils/date-formatter";
 import { Calendar, ChevronLeft, Crown, Zap } from "lucide-react";
 import Link from "next/link";
 
-export function SubscriptionHero() {
-    const planDetails = {
-        planName: "Kurumsal Aylık Plan",
-        renewalDate: "25 Haziran 2025",
-        wordTokens: {
-            total: 1000,
-            used: 185,
-            remaining: 815
-        },
-        imageTokens: {
-            total: 100,
-            used: 50,
-            remaining: 50
-        },
-    }
+interface SubscriptionHeroProps {
+    user: User
+    subscription: Subscription
+    getTokenUsage: () => { used: number; total: number; percentage: number; }
+}
+
+export function SubscriptionHero({ user, subscription, getTokenUsage }: SubscriptionHeroProps) {
+    const { used, total } = getTokenUsage();
 
     return (
         <div className="w-full px-8 flex flex-col ">
@@ -38,7 +34,7 @@ export function SubscriptionHero() {
                             <div className="p-8 flex items-center justify-between bg-success-400 rounded-lg">
                                 <div>
                                     <h1 className="text-white text-body2 font-onest"> Abonelik Planı </h1>
-                                    <h1 className="text-white text-body2 font-onest font-semibold"> {planDetails.planName} </h1>
+                                    <h1 className="text-white text-body2 font-onest font-semibold"> {subscription.name} </h1>
                                 </div>
                                 <Crown className="text-white text-2xl" />
                             </div>
@@ -49,7 +45,7 @@ export function SubscriptionHero() {
                             <div className="p-8 flex items-center justify-between bg-fuchsia-400 rounded-lg">
                                 <div>
                                     <h1 className="text-white text-body2 font-onest"> Yenileme Tarihi </h1>
-                                    <h1 className="text-white text-body2 font-onest font-semibold"> {planDetails.renewalDate} </h1>
+                                    <h1 className="text-white text-body2 font-onest font-semibold"> {formatDate(user?.renewalDate ?? new Date(), { hour: undefined, minute: undefined })} </h1>
                                 </div>
                                 <Calendar className="text-white text-2xl" />
                             </div>
@@ -60,7 +56,7 @@ export function SubscriptionHero() {
                             <div className="p-8 flex items-center justify-between bg-warning-400 rounded-lg">
                                 <div>
                                     <h1 className="text-white text-body2 font-onest"> Kalan token </h1>
-                                    <h1 className="text-white text-body2 font-onest font-semibold"> {planDetails.wordTokens.remaining} / {planDetails.wordTokens.total} </h1>
+                                    <h1 className="text-white text-body2 font-onest font-semibold"> {total - used} / {total} </h1>
                                 </div>
                                 <Zap className="text-white text-2xl" />
                             </div>
