fe3971518284fc3d4f26edf2f2a08b84fbe9d4ef
Yesarib <yesaribaris23@gmail.com>
2025-06-19 15:11:47 +0300

    feat: add change password functionality and enhance reset password error handling, improving user experience with robust password management features

--- src/services/corporate.ts
+++ src/services/corporate.ts
@@ -2,7 +2,7 @@ import { api } from "@/lib/api/api"
 import { Corporate } from "@/types/corporates";
 import { User } from "@/types/user"
 
-export async function getCorporateMembers(corporateId:string): Promise<User[]> {
+export async function getCorporateMembers(corporateId: string): Promise<User[]> {
     try {
         const response = await api.get(`/corporate/${corporateId}/members`)
 
@@ -25,6 +25,26 @@ export async function getCorporates(): Promise<Corporate[]> {
 export async function createCorporate(name: string, domain: string, isActive: boolean): Promise<Corporate> {
     try {
         const response = await api.post('/corporate', { name, domain, isActive });
+
+        return response.data;
+    } catch (error) {
+        throw error
+    }
+}
+
+export async function updateCorporate(corporateId: string, corporate: Corporate): Promise<Corporate> {
+    try {
+        const response = await api.put(`/corporate/${corporateId}`, {
+            name: corporate.name,
+            domain: corporate.domain,
+            totalUsedAnalyses: corporate.totalUsedAnalyses,
+            country: corporate.country,
+            city: corporate.city,
+            state: corporate.state,
+            address: corporate.address,
+            postalCode: corporate.postalCode
+
+        });
 
         return response.data;
     } catch (error) {
