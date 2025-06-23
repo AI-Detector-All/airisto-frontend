0ed711d34a4918da9af0db07e3bdc1a68c46230c
Yesarib <yesaribaris23@gmail.com>
2025-06-23 12:27:16 +0300

    feat: add terms of service page and layout with localization support in English and Turkish, enhancing user understanding and compliance with service agreements

---
+++ src/app/(home)/terms/layout.tsx
@@ -0,0 +1,36 @@
+import { Metadata } from "next";
+import { cookies } from "next/headers";
+
+
+export async function generateMetadata(): Promise<Metadata> {
+    const lang = (await cookies()).get('i18nextLng')?.value || 'tr';
+    const titles = {
+        tr: "Airisto - Kullanım Sözleşmesi",
+        en: "Terms of Service | Airisto",
+    };
+
+    const descriptions = {
+        tr: "Airisto için Hizmet Şartları - Yapay zeka destekli platformumuzu kullanmaya ilişkin hüküm ve koşullarımızı okuyun.",
+        en: "Terms of Service for Airisto - Read our terms and conditions for using our AI-powered platform.",
+    };
+
+    return {
+        title: titles[lang as keyof typeof titles],
+        description: descriptions[lang as keyof typeof descriptions],
+        icons: {
+            icon: "/airisto.ico",
+        }
+    };
+}
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <div className="min-h-screen flex flex-col">
+            <main className="flex-1"> {children} </main>
+        </div>
+    );
+}
