05c015e88a0697b2641b6ed100e13c19e0ad91b3
Yesarib <yesaribaris23@gmail.com>
2025-06-23 12:27:08 +0300

    feat: add privacy policy page and layout with localization support in English and Turkish, improving user transparency and compliance with privacy standards

---
+++ src/app/(home)/privacy/layout.tsx
@@ -0,0 +1,36 @@
+import { Metadata } from "next";
+import { cookies } from "next/headers";
+
+
+export async function generateMetadata(): Promise<Metadata> {
+    const lang = (await cookies()).get('i18nextLng')?.value || 'tr';
+    const titles = {
+        tr: "Airisto - Gizlilik Politikası",
+        en: "Privacy Policy | Airisto",
+    };
+
+    const descriptions = {
+        tr: "Airisto'nun Gizlilik Politikası - Kişisel bilgilerinizin toplanma, kullanımı ve korunumu hakkında bilgi almak istiyorsanız.",
+        en: "Privacy Policy for Airisto - Learn how we collect, use, and protect your personal information.",
+    };
+
+    return {
+        title: titles[lang as keyof typeof titles],
+        description: descriptions[lang as keyof typeof descriptions],
+        icons: {
+            icon: "/airisto.ico",
+        }
+    };
+}
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <div className="min-h-screen flex flex-col">
+            <main className="flex-1"> {children} </main>
+        </div>
+    );
+}
